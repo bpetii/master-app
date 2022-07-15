1388c828516791a5a3d376fd092eb970e4b6e743
peter.biro <peter.biro@oliasoft.com>
2022-07-15 23:20:22 +0200

    setup session for login

--- server/server.js
+++ server/server.js
@@ -1,5 +1,29 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
+const authRouter = require('./routers/authRouters');
+const session = require('session');
+require("dotenv").config();
+
+app.use(helmet());
+app.use(cors())
+
+app.use(express.json());
+
+app.use(session({
+    secret: process.env.COOKIE_SECRET,
+    credentials:true,
+    name: 'sid',
+    resave: false,
+    saveUnititialized: false,
+    cookie: {
+        secure: process.env.ENVIRONMENT === "production",
+        httpOnly: true,
+        sameSite: process.env.ENVIRONMENT === "production" ? "none" : "lax",
+    } 
+}))
+
+app.use("/auth", authRouter)
 
 const port = 4000
 
