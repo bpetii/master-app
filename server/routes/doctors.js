f6bf9f551f6760de613c810d8224064baa71492b
peter.biro <peter.biro@oliasoft.com>
2022-10-21 23:10:25 +0200

    add doctor router

---
+++ server/routes/doctors.js
@@ -0,0 +1,17 @@
+const router = require('express').Router();
+const pool = require('../db');
+const authorization = require('../middleware/authorization')
+
+router.get("/doctors", authorization, async (req, res) => {
+  console.log(req);
+  try {
+    const doctors = await pool.query("SELECT id, name, expertise, city FROM doctors",
+		[]);
+		res.json(doctors.rows);
+  } catch (err){
+    console.error(err.message);
+    res.status(500).json("Server Error");
+  }
+})
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const app = express();
 const auth = require('./routes/auth');
 const users = require('./routes/users');
-const dashboardRouter = require('./routes/dashboard');
+const doctors= require('./routes/doctors');
 require("dotenv").config();
 
 app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
@@ -12,7 +12,7 @@ app.use(express.json());
 
 app.use('/api/users', users);
 app.use('/api/auth', auth);
-app.use('/api/dashboard', dashboardRouter)
+app.use('/api/doctors', doctors)
 
 
 const port = process.env.PORT || 4000
