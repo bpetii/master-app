98b698d6876d4dda462d5800b46cfad1efd57e32
peter.biro <peter.biro@oliasoft.com>
2022-10-21 23:10:11 +0200

    remove loggedin prop

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -13,13 +13,13 @@ router.post('/', async (req, res) => {
       "SELECT id, name, passhash, email, issecretary FROM users u WHERE u.email=$1 AND u.issecretary=$2",
       [email, isSecretary])
   
-      if (!user.rowCount) return res.status(400).json({loggedIn: false, status: "Invalid email or password", username: null});
+      if (!user.rowCount) return res.status(400).json({ status: "Invalid email or password", username: null });
         
       const isValidPassword = await bcrypt.compare(password, user.rows[0].passhash);
-      if(!isValidPassword) return res.status(400).json({loggedIn: false, status: "Invalid email or password", ema: null})
+      if(!isValidPassword) return res.status(400).json({ status: "Invalid email or password", ema: null })
 
       const access_token = jwtGenerator(user.rows[0].id);
-      res.header('x-auth-token', access_token).send({access_token, loggedIn: true, user: omit(user.rows[0], ['passhash'])}) 
+      res.header('x-auth-token', access_token).send({ access_token, user: omit(user.rows[0], ['passhash']) }) 
   });
 
   module.exports = router;
