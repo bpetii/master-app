538cabc1307a3c019617c46b6ec8293f53a3be08
peter.biro <peter.biro@oliasoft.com>
2022-11-10 15:13:30 +0100

    fix langauge picker

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,15 +8,38 @@ import {initReactI18next} from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import {translationsEn, translationsFa, translationsHu} from './translations';
 
+const languageList = [
+  {
+    value: 'en',
+    translation: translationsEn,
+  },
+  {
+    value: 'fa',
+    translation: translationsFa,
+  },
+  {
+    value: 'hu',
+    translation: translationsHu,
+  },
+];
+
+
+const languageResources = (langs) => {
+  const obj = {};
+  langs.forEach(lng => {
+    obj[lng.value] = {
+      translation: lng.translation,
+    };
+  });
+  return obj;
+};
+
+
 i18n
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    resources: {
-        en: {translation: translationsEn},
-        fa: {translation: translationsFa},
-        hu: {trasnlation: translationsHu},
-      },
+    resources: languageResources(languageList),
     debug:true,
     lng: "en",
     fallbackLng: "en",
